96b7eece1f0968989698e89bd6a6c106785ea419
strangeal <bajerijustice@gmail.com>
2022-11-15 21:11:27 -0500

    display reversed rocket

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
 const NotFound = () => (
   <div className="not__found">
     <h2>404</h2>
     <p>Page not found</p>
 
-    <Link to="/">Back to Home</Link>
+    <Link to="/">
+      <IoIosArrowRoundBack />
+      Back to Home
+    </Link>
   </div>
 );
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+const Profile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+
+  const myRocketList = rockets.filter((rocks) => rocks.reserved);
+  return (
+    <div className="profile">
+      <h2>My Rockets</h2>
+      {myRocketList.length === 0 ? (
+        <div className="no__reservations">
+          <p>No Reservations Made</p>
+          <div className="make__reservations">
+            <Link to="/">
+              <IoIosArrowRoundBack />
+              Make Reservations
+            </Link>
+          </div>
+        </div>
+
+      ) : (
+        <ul>
+          {myRocketList.map(({ id, rocketName }) => (
+            <li key={id}>{rocketName}</li>
+          ))}
+        </ul>
+      )}
+
+    </div>
+  );
+};
 
 export default Profile;
