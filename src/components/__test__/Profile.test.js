6824cc96e76b8f849bd326a869f01d89ec7e8026
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-17 15:30:58 +0100

    Fix eslint errors

--- src/components/__test__/Profile.test.js
+++ src/components/__test__/Profile.test.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
-import { renderWithProviders } from '../../utils/testUtils';
+import { screen } from '@testing-library/react';
+import renderWithProviders from '../../utils/testUtils';
 import Profile from '../Profile';
 
 describe('Testing the Profile component', () => {
-  it("Checking that the Missions section renders correctly: ", () => {
-    renderWithProviders(<Profile/>);
+  it('Checking that the Missions section renders correctly: ', () => {
+    renderWithProviders(<Profile />);
     const missionsHeader = screen.getByText('My Missions');
     expect(missionsHeader).toBeTruthy();
   });
 
-  it("Checking that the Rockets section renders correctly: ", () => {
-    renderWithProviders(<Profile/>);
+  it('Checking that the Rockets section renders correctly: ', () => {
+    renderWithProviders(<Profile />);
     const rocketsHeader = screen.getByText('My Rockets');
     expect(rocketsHeader).toBeTruthy();
   });
 
-  it("Checking that the Rockets section renders correctly: ", () => {
-    renderWithProviders(<Profile/>);
+  it('Checking that the Rockets section renders correctly: ', () => {
+    renderWithProviders(<Profile />);
     const rocketsHeader = screen.getByText('My Rockets');
     expect(rocketsHeader).toBeTruthy();
   });
 
-  it("Checking that no missoins are joined", () => {
-    renderWithProviders(<Profile/>);
-    const missionsMsg= screen.getByText('No Missions Joined');
+  it('Checking that no missoins are joined', () => {
+    renderWithProviders(<Profile />);
+    const missionsMsg = screen.getByText('No Missions Joined');
     expect(missionsMsg).toBeTruthy();
   });
 
-  it("Checking that no missoins are joined", () => {
-    renderWithProviders(<Profile/>);
-    const rocketsMsg= screen.getByText('No Missions Joined');
+  it('Checking that no missoins are joined', () => {
+    renderWithProviders(<Profile />);
+    const rocketsMsg = screen.getByText('No Missions Joined');
     expect(rocketsMsg).toBeTruthy();
   });
 });
--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { BrowserRouter } from 'react-router-dom';
 
-import missionsReducer from "../redux/missions/missions";
-import rocketsReducer from "../redux/rockets/rocketSlice";
+import missionsReducer from '../redux/missions/missions';
+import rocketsReducer from '../redux/rockets/rocketSlice';
 
 const rootState = {
   missions: [
     {
-      mission_name: "Thaicom",
-      mission_id: "9D1B7E0",
+      mission_name: 'Thaicom',
+      mission_id: '9D1B7E0',
       description:
-        "Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.",
+        'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
     },
   ],
 };
-export function renderWithProviders(
+export default function renderWithProviders(
   ui,
   {
     preloadedState = rootState,
@@ -26,9 +26,9 @@ export function renderWithProviders(
       preloadedState,
     }),
     ...renderOptions
-  } = {}
+  } = {},
 ) {
-  function Wrapper({ children }) {
+  function Wrapper({ children }) { /* eslint-disable-line */
     return (
       <BrowserRouter>
         <Provider store={store}>{children}</Provider>
